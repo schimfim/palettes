6233707c6ba81fb81bd8fe6b6078d555ef3d4fda
uncleWalter <schimfim@mac.com>
2014-02-01 15:06:54 -0800

    Auto commit by giteasy

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pals import pals
 
 # config
 slope = 20.0 # small=large impact
-focus = 1.3 # small=narrow range
+focus = 1.19 # small=narrow range
 palettes.xf = 1.0
 palettes.sigma = 10.0
 palettes.stats = True  
@@ -16,12 +16,13 @@ rgb = [(1.0,0.0,0.0),(0.0,1.0,0.0),(0.0,0.0,1.0)]
 
 imgs = palettes.load_all('orig')
 ps = pals[0]
-ims = imgs[2]
+ims = imgs[0]
 palettes.draw_palette(ps)
 out = None 
 
 def start():
 	global out
+	focus = palettes.analyse(ims, ps, 10000)
 	out = palettes.adapt(ims, ps, slope, focus) 
 
 if __name__ == '__main__':
