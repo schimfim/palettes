59f4cdf3637ff9835c26dd476c4eb95724bb2839
uncleWalter <schimfim@mac.com>
2014-02-04 14:01:45 -0800

    Auto commit by giteasy

--- test.py
+++ test.py
@@ -4,25 +4,22 @@ from pals import pals
 
 # config
 slope = 20.0 # small=large impact
-focus = 1.19 # small=narrow range
-palettes.xf = 1.0
-palettes.sigma = 10.0
-palettes.stats = True  
-print 'xf={}, slp={}, fcs={}, sig={}'.format(palettes.xf,slope,focus,palettes.sigma)
+palettes.xf = 0.1
+palettes.defcol = (0.1,0.1,0.1)
+palettes.stats = True 
 
-blu = [(0.0,0.0,1.0)]
-red = [(1.0,0.0,0.0)]
-rgb = [(1.0,0.0,0.0),(0.0,1.0,0.0),(0.0,0.0,1.0)]
+print 'xf={}, slp={}'.format(palettes.xf,slope)
 
 imgs = palettes.load_all('orig')
-ps = pals[0]
-ims = imgs[0]
-palettes.draw_palette(ps)
+pnam = 'rgb'
+ps = pals[pnam]
+ims = imgs[2]
+palettes.draw_palette({pnam:ps})
 out = None 
 
 def start():
 	global out
-	focus = palettes.analyse(ims, ps, 10000)
+	focus = palettes.analyse(ims, ps, gain=0.5, nperc=0.3, k=10000)
 	out = palettes.adapt(ims, ps, slope, focus) 
 
 if __name__ == '__main__':
