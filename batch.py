32054bd2171f84e0501fdbcc14209e4973bdea86
uncleWalter <schimfim@mac.com>
2014-02-04 14:01:41 -0800

    Auto commit by giteasy

--- batch.py
+++ batch.py
@@ -6,8 +6,7 @@ from pals import pals
 slope = 20.0 # small=large impact
 focus = 1.0 # small=narrow range
 palettes.xf = 1.0
-palettes.sigma = 10.0
-palettes.stats = True  
+palettes.sigma = 10.0  
 print 'xf={}, slp={}, fcs={}, sig={}'.format(palettes.xf,slope,focus,palettes.sigma)
 
 blu = [(0.0,0.0,1.0)]
@@ -16,11 +15,13 @@ rgb = [(1.0,0.0,0.0),(0.0,1.0,0.0),(0.0,0.0,1.0)]
 
 imgs = palettes.load_all('orig')
 npals = [palettes.calc_palette(pal_img, 5) for pal_img in imgs]
-ps = pals[0]
-ps = rgb
-ims = imgs[2]
 palettes.draw_palette(*npals)
 
-out = [palettes.adapt(img, pal, slope, focus) for img in imgs for pal in npals]
-#palettes.save_all(out,'out')
+out = []
+for img in imgs:
+	for pal in npals:
+		focus = palettes.analyse(img, pal, 10000)
+		out.append(palettes.adapt(img, pal, slope, focus))
+	
+palettes.save_all(out,'out')
 
